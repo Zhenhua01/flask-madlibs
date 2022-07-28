b025f1c97575bdbb283036b44ef8714d5affb86b
Zhenhua <zhenhua1109@yahoo.com>
2022-07-27 21:28:52 -0700

    added notes

--- app.py
+++ app.py
@@ -11,18 +11,18 @@ debug = DebugToolbarExtension(app)
 @app.get("/")
 def show_page():
     """send prompts to questions.html"""
-    return render_template("questions.html", prompt_list=silly_story.prompts)
+    return render_template("questions.html", prompt_list = silly_story.prompts)
 
 @app.get("/results")
 def show_results():
     """get inputs and show story with inputs"""
+
+    story_text = silly_story.generate(request.args)
+
+    return render_template("results.html", story = story_text)
+
+    #use above since request.args return 'dictionary' object of all args in query
     """answer_list = {}
     for prompt in silly_story.prompts:
         prompt_input = request.args.get(prompt)
         answer_list[prompt] = answer_list.get(prompt,prompt_input) """
-
-    story_text = silly_story.generate(request.args)
-
-    return render_template("results.html", story=story_text)
-
-
