627e6e3dbc92559c367641f448d4ac87d31e2192
Billyhrussell <billy.hrussell@gmail.com>
2022-07-27 17:25:41 -0700

    fin

--- app.py
+++ app.py
@@ -7,3 +7,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def show_page():
+    """send prompts to questions.html"""
+    return render_template("questions.html", prompt_list=silly_story.prompts)
+
+@app.get("/results")
+def show_results():
+    """get inputs and show story with inputs"""
+    """answer_list = {}
+    for prompt in silly_story.prompts:
+        prompt_input = request.args.get(prompt)
+        answer_list[prompt] = answer_list.get(prompt,prompt_input) """
+
+    story_text = silly_story.generate(request.args)
+
+    return render_template("results.html", story=story_text)
+
+
